241eab230e147eaf0d7d1117b54c1fb6a5f8f651
Romulo <romulogomes13@hotmail.com>
2018-10-24 13:19:47 -0300

    add component Table

---
+++ src/components/Table.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class Table extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            orientadorSelecionado : ''
+        }
+    }
+
+    render() {
+        const { dados } = this.props;
+        if(dados.length) {
+            const dado = dados[0];
+            return (
+                
+                <div className="col-11">
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                { dado ? Object.keys(dados[0]).map(chave =>  <th className="titulo_table" key={chave}>{[chave]}</th> ) : false }
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { dados.map(dado => 
+                                <tr key={dado.id} className={`${dado.id === this.props.selecionado.id ? 'table-primary' : ''}`} onClick={() => this.props.setSelecionado(dado)}>
+                                    { Object.keys(dado).map(chave =>
+                                        <td key={chave}>{dado[chave]}</td>    
+                                    )}
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>            
+                </div>
+            );
+        }
+        else{
+            return ( "" )
+        }
+    }
+}
+
+export default Table;
